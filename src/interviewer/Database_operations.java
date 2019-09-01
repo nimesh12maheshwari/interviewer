559a823014a4a902538d0d0833e93f0aefc99836
Aayush Agarwal <aayushagarwalmnnit@gmail.com>
2019-09-02 00:06:38 +0530

    5 files

---
+++ src/interviewer/Database_operations.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package interviewer;
+import java.sql.*;
+/**
+ *
+ * @author Aayush
+ */
+public class Database_operations 
+{
+    void read()
+    {
+        
+    }
+    void write(Student_initials si) throws SQLException, ClassNotFoundException
+    {
+        System.out.println("executed 5.1\n");
+        Class.forName("com.mysql.jdbc.Driver");
+        System.out.println("executed 5.2\n");
+        Connection con=DriverManager.getConnection( "jdbc:mysql://localhost:3306/intercode","root","root");
+        System.out.println("executed 5.3\n");
+        Statement stmt=con.createStatement();  
+        System.out.println("executed 5.4\n");
+        String query;
+        query="insert into student values('"+si.clg+"','"+si.reg_no+"','"+si.name+"','"+si.branch+"',"+si.score+",'"+si.notes+"');";        
+        System.out.println("executed 5.5\n");
+        System.out.println(query);
+        stmt.executeUpdate(query);  
+    }
+}
---
+++ src/interviewer/Student_initials.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package interviewer;
+
+/**
+ *
+ * @author Aayush
+ */
+public class Student_initials 
+{
+    String clg;
+    String reg_no;
+    String name;
+    String branch;
+    int score;
+    String notes;
+    Student_initials()
+    {
+        clg="";
+        reg_no="";
+        name="";
+        branch="";
+        score=0;
+        notes="nothing";
+    }
+}
