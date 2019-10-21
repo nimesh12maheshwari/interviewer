0f4a414e236077de9f89f0fbfc2e1a47b96d92ad
Pranjal <pranjalpandey77@gmail.com>
2019-10-21 19:46:54 +0530

    Hactoberfest3

--- src/interviewer/Database_operations.java
+++ src/interviewer/Database_operations.java
@@ -12,9 +12,20 @@ import java.sql.*;
  */
 public class Database_operations 
 {
-    void read()
+    void read() throws SQLException, ClassNotFoundException
     {
-        
+        System.out.println("executed 5.1\n");
+        Class.forName("com.mysql.jdbc.Driver");
+        System.out.println("executed 5.2\n");
+        Connection con=DriverManager.getConnection( "jdbc:mysql://localhost:3306/intercode","root","kashyap@99");
+        System.out.println("executed 5.3\n");
+        Statement stmt=con.createStatement();  
+        System.out.println("executed 5.4\n");
+        String query;
+        query="select * from student;";        
+        System.out.println("executed 5.5\n");
+        System.out.println(query);
+        stmt.executeUpdate(query);
     }
     void write(Student_initials si) throws SQLException, ClassNotFoundException
     {
